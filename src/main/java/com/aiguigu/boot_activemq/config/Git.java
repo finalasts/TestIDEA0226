783406e98a5fc6966635e70890dbf688b06dbdf7
67 <67@123.com>
2020-02-26 21:25:47 +0800

    insert Git Class

---
+++ src/main/java/com/aiguigu/boot_activemq/config/Git.java
@@ -0,0 +1,13 @@
+package com.aiguigu.boot_activemq.config;
+
+import java.sql.SQLOutput;
+
+/**
+ * @author WangPengfei
+ * @create 2020-02-26 21:24
+ */
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("test git");
+    }
+}
